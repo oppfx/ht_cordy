0ce19038e6db8d9fd6de9e36281c9cf21710dd3f
joker <>
2021-02-27 23:32:23 +0800

    first commit

---
+++ js/language.js
@@ -0,0 +1,574 @@
+var indexhtml = {
+    home:{
+        'zh-cn':'首页',
+        'en-us':'Home'
+    },
+    add:{
+        'zh-cn':'添加',
+        'en-us':'Add'
+    },
+    farms:{
+        'zh-cn':'农场',
+        'en-us':'Farms'
+    },
+    clear:{
+        'zh-cn':'清除',
+        'en-us':'Clear'
+    },
+    referral:{
+        'zh-cn':'邀请',
+        'en-us':'Referral'
+    },
+    about:{
+        'zh-cn':'关于',
+        'en-us':'About'
+    },
+    migrate:{
+        'zh-cn':'迁移',
+        'en-us':'Migrate'
+    },
+    balance:{
+        'zh-cn':'余额',
+        'en-us':'Balance'
+    },
+    mgtp1:{
+        'zh-cn':'迁移到火币智能链 (Heco)',
+        'en-us':'Migrate to Huobi Eco (Heco) Chain'
+    },
+    mgdf:{
+        'zh-cn':'提示: 不要填写交易所地址',
+        'en-us':'Tip: Do not fill in the exchange address'
+    },
+    hecoaddr:{
+        'zh-cn':'你的 Heco 钱包地址',
+        'en-us':'Heco Wallet Address'
+    },
+
+    exchange:{
+        'zh-cn':'交易所',
+        'en-us':' Exchange'
+    },
+    myacc:{
+        'zh-cn':'我的账户',
+        'en-us':'My Account'
+    },
+    conwallet:{
+        'zh-cn':'连接钱包',
+        'en-us':'Connect Wallet'
+    },
+    getairdrop:{
+        'zh-cn':'获取 NFT 空投',
+        'en-us':'Get Airdrop'
+    },
+    claimnft:{
+        'zh-cn':'认领NFT',
+        'en-us':'Claim NFT'
+    },
+    hpt1:{
+        'zh-cn':'随时验证令牌，随时获得奖励',
+        'en-us':'Verfiy Tokens , Get Rewards Instantly.'
+    },
+    hpt2:{
+        'zh-cn':'今天是挖虫草的好日子',
+        'en-us':'It\'s a good day to mine Cordyceps.'
+    },
+    tsupply:{
+        'zh-cn':'总供应量',
+        'en-us':'Total Supply'
+    },
+    cliquidty:{
+        'zh-cn':'当前流通量',
+        'en-us':'Current Liquidity'
+    },
+    mybal:{
+        'zh-cn':'我的账户',
+        'en-us':'My Balance'
+    },
+    bonpool:{
+        'zh-cn':'分红池',
+        'en-us':'Bonus Pool'
+    },
+    entamount:{
+        'zh-cn':'输入数量',
+        'en-us':'Enter amount'
+    },
+
+    fpt1:{
+        'zh-cn':'选择一个池子',
+        'en-us':'Select a pool'
+    },
+    fpt2:{
+        'zh-cn':'通过质押令牌获得虫草',
+        'en-us':'Earn Cordyceps by staking tokens'
+    },
+    fmpt1:{
+        'zh-cn':'即可获取虫草',
+        'en-us':'and earn Cordyceps'
+    },
+    fmpt2:{
+        'zh-cn':'当前周期奖励',
+        'en-us':'Current period rewards'
+    },
+    fmpt3:{
+        'zh-cn':'当前收割利率',
+        'en-us':'Current harvest Rate'
+    },
+    fmpt4:{
+        'zh-cn':'已获取虫草',
+        'en-us':'Cordyceps earned'
+    },
+    harvest:{
+        'zh-cn':'　收割　',
+        'en-us':'Harvest'
+    },
+    stake:{
+        'zh-cn':'　质押　',
+        'en-us':'Stake'
+    },
+    cpt1:{
+        'zh-cn':'清除那些破坏规则的玩家',
+        'en-us':'Clear the user who breaks the rules'
+    },
+    cpt2:{
+        'zh-cn':'你已清除: ',
+        'en-us':'You cleared: '
+    },
+    package:{
+        'zh-cn':'包裹',
+        'en-us':'PACKAGE'
+    },
+    cordynft:{
+        'zh-cn':'虫草 NFT',
+        'en-us':'CORDYCEPS NFT'
+    },
+    install:{
+        'zh-cn':'安装',
+        'en-us':'Install'
+    },
+    tmt1:{
+        'zh-cn':'请安装TronLink插件来玩！',
+        'en-us':'Please install TronLink to play !'
+    },
+    reflink:{
+        'zh-cn':'邀请链接',
+        'en-us':'Refer Link'
+    },
+    rmt1:{
+        'zh-cn':'提现利率的10%作为奖励',
+        'en-us':'10% of the withdrawal rate as a reward'
+    },
+    rmt2:{
+        'zh-cn':'获取ID失败，请先注册',
+        'en-us':'Failed to get ID, please register first'
+    },
+    copy:{
+        'zh-cn':'复制',
+        'en-us':'Copy'
+    },
+    regis:{
+        'zh-cn':'　注册　',
+        'en-us':'Register'
+    },
+    refbon:{
+        'zh-cn':'邀请奖励',
+        'en-us':'Refer Rewards'
+    },
+    refnum:{
+        'zh-cn':'你已邀请:',
+        'en-us':'Referrals:'
+    },
+    withdraw:{
+        'zh-cn':'　提现　',
+        'en-us':'Withdraw'
+    },
+    corbal:{
+        'zh-cn':'虫草余额',
+        'en-us':'Cordyceps Balance'
+    },
+    close:{
+        'zh-cn':'　关闭　',
+        'en-us':'Close'
+    },
+    select:{
+        'zh-cn':'选择',
+        'en-us':'Select'
+    },
+    max:{
+        'zh-cn':'最大',
+        'en-us':'Max'
+    },
+    pmt1:{
+        'zh-cn':'概率产出',
+        'en-us':'Probability output'
+    },
+    pmt2:{
+        'zh-cn':'赠送给你的朋友',
+        'en-us':'Send to your friend'
+    },
+    open:{
+        'zh-cn':'打开',
+        'en-us':'Open'
+    },
+    send:{
+        'zh-cn':'赠送',
+        'en-us':'Send'
+    },
+    name:{
+        'zh-cn':'名称',
+        'en-us':'Name'
+    },
+    level:{
+        'zh-cn':'等级',
+        'en-us':'Level'
+    },
+    quality:{
+        'zh-cn':'品质',
+        'en-us':'Quality'
+    },
+    birth:{
+        'zh-cn':'生日',
+        'en-us':'Birth'
+    },
+    
+    prompt:{
+        'zh-cn':'提示',
+        'en-us':'Prompt'
+    },
+    about1:{
+        'zh-cn':'致所有Defi玩家',
+        'en-us':'To all Defi miners'
+    },
+    about2:{
+        'zh-cn':'曾经的Defi让所有人都为之疯狂，它如同钻石一般，所有人都趋之若鹜，而现在，它好像褪去了原先璀璨的锋芒，变得有些暗淡无光，它会蜕变出新的生命吗？',
+        'en-us':'Defi used to make everyone crazy. It\'s like a diamond, and everyone is flocking to it. Now, it seems that it has faded its original bright edge and become a little dim. Will it glow with new life?'
+    },
+    about3:{
+        'zh-cn':'我不知道.',
+        'en-us':'I don\'t know.'
+    },
+    about4:{
+        'zh-cn':'但是，cordyceps的到来却是一件无比令人兴奋的事情，它是波场上首个验资挖矿的Defi实验项目，团队由早期的以太坊开发者组建，有着丰富的技术经验，同时，我们有着共同的目标，那就是给同质化的Defi生态带来不一样的内容，希望再一次让Defi重新散发全新的生命，同时，我们注意到，越来越多的欺诈型项目无情地掏空了用户的钱包，因此，我们对用户资金的安全提出了新的要求，我们重写了数十份合约，在未来，即便虫草合约出现了未知的漏洞风险，我们也希望，用户的token依然在用户的钱包里，在任何时刻，它都是安全的',
+        'en-us':'However, the arrival of Cordyceps is a very exciting thing. It is Tron first Defi experiment project to verify the token and mine. The team is established by the early Ethereum developers and has extensive development experience. At the same time, we have a common goal, that is to bring different contents to the homogeneous Defi ecology, and hope to let Defi redistribute new life again. At the same time, we have noticed that more and more fraudulent projects have ruthlessly emptied users\' wallets. Therefore, we have put forward new requirements for the security of users\' funds. We have rewritten dozens of contracts. In the future, even if there is some unknown risk bugs in the Cordyceps contract, we also hope that the user\'s token will still be in the user\'s wallet. At any time, it will be safe.'
+    },
+    about5:{
+        'zh-cn':'当然，虫草的获取也是免费的.',
+        'en-us':'Of course, the acquisition of Cordyceps is also free.'
+    },
+    about6:{
+        'zh-cn':'经过一个多月的开发与调试, 虫草即将到来！',
+        'en-us':'After more than a month of development and debugging, Cordyceps is coming soon!'
+    },
+    about7:{
+        'zh-cn':'开始之前，我们有个礼物，那就是将送给每一位玩家一份虫草NFT空投.',
+        'en-us':'Before we start, we have a gift, which is to give each player a Cordyceps NFT airdrop.'
+    },
+    about8:{
+        'zh-cn':'0x1 虫草令牌是什么？',
+        'en-us':'0x1 What is the Cordyceps token?'
+    },
+    about9:{
+        'zh-cn':'虫草令牌是一种全新的实验性ERC-20 Defi令牌，总量为30000，团队无预留股份，无预挖，将全部由YFI形式产出，同时会有分红机制',
+        'en-us':'Cordyceps token is a new experimental ERC-20 Defi token with a total amount of 30000.The team has no pre-mine, no founder shares.All tokens will be distributed in the sprit of YFI,and there will be a dividend mechanism.'
+    },
+    about10:{
+        'zh-cn':'0x2 虫草令牌如何获得？',
+        'en-us':'0x2 how to get the Cordyceps token?'
+    },
+    about11:{
+        'zh-cn':'虫草挖矿很简单，只需要质押LP令牌数量即可开启挖矿',
+        'en-us':'Cordyceps mining is very simple, mining can be started only by staking the number of LP tokens.'
+    },
+    about14:{
+        'zh-cn':'0x3 虫草令牌会上架Mdex吗？',
+        'en-us':'0x3 Will Cordyceps token be listed on Mdex?'
+    },
+    about15:{
+        'zh-cn':'是的',
+        'en-us':'Yes'
+    },
+    about16:{
+        'zh-cn':'0x4 关于提现利率',
+        'en-us':'0x4 About withdrawal rate'
+    },
+    about17:{
+        'zh-cn':'提现利率将会从75%降至15%，5天时间线性下降，从验证令牌挖矿开始计算，当然，提现奖励也会重新刷新时间，这种机制将会让虫草价格不至于剧烈波动，同时有利于降低作恶矿工收益，提高作恶成本',
+        'en-us':'The withdrawal rate will be reduced from 75% to 15%, and it will decrease linearly in 5 days. Starting from the verification token mining, of course, the withdrawal reward will refresh the time again. This mechanism will prevent the price of Cordyceps from drastic fluctuations, and at the same time, it will reduce the income of evil miners and increase the cost of doing evil.'
+    },
+    about18:{
+        'zh-cn':'0x5 关于分红池',
+        'en-us':'0x5 About bonus pool'
+    },
+    about19:{
+        'zh-cn':'用户提现虫草奖励将会按照提现利率扣除一部分给到分红池，其中10%将分给你的邀请者，30%分给项目团队用于开发与运营，60%将归入分红池，如果你没有邀请人，那就有70%归入分红池，分红池内的奖励将不定期分给虫草持有者',
+        'en-us':'Users will deduct part of the Cordyceps reward to the dividend pool according to the withdrawal rate, of which 10% will be allocated to your invitees, 30% will be allocated to the project team for development and operation, and 60% will be included in the bonus pool. If you have no invitees, 70% will be included in the dividend pool, and the rewards in the dividend pool will be distributed to Cordyceps holders from time to time.'
+    },
+    about20:{
+        'zh-cn':'0x6 关于虫草NFT',
+        'en-us':'0x6 About Cordyceps NFT'
+    },
+    about21:{
+        'zh-cn':'虫草NFT很快就会上线，并开启NFT挖矿，要注意的是，虫草NFT将会全部空投给玩家，团队不预留',
+        'en-us':'Cordyceps NFT will be coming soon, and start NFT mining. It should be noted that all Cordyceps NFT will be airdropped to users, and the team will not reserve it.'
+    },
+    about22:{
+        'zh-cn':'0x7 未来计划',
+        'en-us':'0x7 Future plans'
+    },
+    about23:{
+        'zh-cn':'我们已经有了初步的想法，就是关于[挖矿型NFT交易所]的大致架构，它将不同于传统交易所，一起期待吧',
+        'en-us':'We have a preliminary idea about the general structure of the Mining-NFT exchange. It will be different from the traditional exchange. Let\'s look forward to it.'
+    },
+  }
+  
+
+
+
+  var appjs ={
+    version:{
+        'zh-cn':'1.03',
+        'en-us':'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890.,/#'
+    },
+    tip:{
+        'zh-cn':'提示: ',
+        'en-us':'Tip: '
+    },
+    regis1:{
+        'zh-cn':'注册中...',
+        'en-us':'Registering...'
+    },
+    regis2:{
+        'zh-cn':'注册成功，获取ID中...',
+        'en-us':'Registration successful, getting ID...'
+    },
+    regisfail:{
+        'zh-cn':'注册失败',
+        'en-us':'Registration failure'
+    },
+    mgdf:{
+        'zh-cn':'提示: 不要填写交易所地址',
+        'en-us':'Tip: Do not fill in the exchange address'
+    },
+    mgcheck1:{
+        'zh-cn':'正在检测历史迁移...',
+        'en-us':'Checking historical migration...'
+    },
+    mgcheck2:{
+        'zh-cn':'检测完毕...',
+        'en-us':'Check it out...'
+    },
+    mgcheck3:{
+        'zh-cn':'不要关闭本页面，等待验证令牌...',
+        'en-us':'Do not close this page and wait for validation token...'
+    },
+    mgcheck4:{
+        'zh-cn':'不要关闭本页面，令牌正在迁移中...',
+        'en-us':'Do not close this page and the token is migrating...'
+    },
+    mgsucc:{
+        'zh-cn':'迁移令牌已发送，请稍等片刻查询火币账户...',
+        'en-us':'The migration token has been sent. Please wait for a moment to check the Huobi account...'
+    },
+    erraddr:{
+        'zh-cn':'警告：输入地址错误 !',
+        'en-us':'Warning: wrong input address!'
+    },
+    mgappro:{
+        'zh-cn':'申请授权中...',
+        'en-us':'Applying for authorization...'
+    },
+    mgtoken1:{
+        'zh-cn':'令牌迁移中...',
+        'en-us':'Token migrating...'
+    },
+    copy:{
+        'zh-cn':'复制',
+        'en-us':'Copy'
+    },
+    copysucc:{
+        'zh-cn':'复制成功',
+        'en-us':'Copy success'
+    },
+    copyfail:{
+        'zh-cn':'复制失败',
+        'en-us':'Copy fail'
+    },
+    select:{
+        'zh-cn':'　选择　',
+        'en-us':'Select'
+    },
+    end:{
+        'zh-cn':'结束',
+        'en-us':'Ended'
+    },
+    earn:{
+        'zh-cn':'获取',
+        'en-us':'Earn'
+    },
+    staket:{
+        'zh-cn':'质押',
+        'en-us':'Stake'
+    },
+    verifyt:{
+        'zh-cn':'验证',
+        'en-us':'Verify'
+    },
+    stake:{
+        'zh-cn':'　质押　',
+        'en-us':'Stake'
+    },
+    verify:{
+        'zh-cn':'　验证　',
+        'en-us':'Verify'
+    },
+    comson:{
+        'zh-cn':'即将开始',
+        'en-us':'Coming soon'
+    },
+    halve:{
+        'zh-cn':'减产',
+        'en-us':'halve'
+    },
+    sending:{
+        'zh-cn':'发送中...',
+        'en-us':'Sending...'
+    },
+    sendsucc:{
+        'zh-cn':'发送成功，请稍后刷新页面...',
+        'en-us':'Send success! please reload this page later...'
+    },
+    sendfail:{
+        'zh-cn':'发送失败，请稍后重试...',
+        'en-us':'Send fail! try again...'
+    },
+    addrerror:{
+        'zh-cn':'无效的地址',
+        'en-us':'Incorrect wallet address'
+    },
+    opensucc:{
+        'zh-cn':'打开成功，请稍后刷新页面...',
+        'en-us':'Open success,please reload this page later...'
+    },
+    opening:{
+        'zh-cn':'打开中...',
+        'en-us':'Opening...'
+    },
+    openfail:{
+        'zh-cn':'打开失败，请稍后重试...',
+        'en-us':'Open fail! try again...'
+    },
+    nopack:{
+        'zh-cn':'你还没有任何包裹',
+        'en-us':'You don\'t have package yet'
+    },
+    nonft:{
+        'zh-cn':'你还没有任何NFT',
+        'en-us':'You don\'t have NFT yet'
+    },
+    unstake:{
+        'zh-cn':'　取回　',
+        'en-us':'Unstake'
+    },
+    staked:{
+        'zh-cn':'已质押',
+        'en-us':'staked'
+    },
+    verified:{
+        'zh-cn':'已验证',
+        'en-us':'verified'
+    },
+    Verified:{
+        'zh-cn':'已验证',
+        'en-us':'Verified'
+    },
+    update:{
+        'zh-cn':'　更新　',
+        'en-us':'Update'
+    },
+    max:{
+        'zh-cn':'已达最大',
+        'en-us':'Maximum'
+    },
+    wait:{
+        'zh-cn':'等待...',
+        'en-us':'Wait...'
+    },
+    succ:{
+        'zh-cn':'　成功　',
+        'en-us':'Success'
+    },
+
+
+    close:{
+        'zh-cn':'关闭',
+        'en-us':'Close'
+    },
+    
+  }
+
+
+
+
+  
+  var i18nData={
+    indexhtml:indexhtml,
+    appjs:appjs
+  }
+  var I18N = {
+    DATA : i18nData,
+    language: 'zh-cn',
+    setLanguage: function(language){
+        this.language = language;
+    }
+
+    ,parse: function(str, pageName){
+        var _this = this;
+        if(!pageName){
+            console.error('I18N解析失败，原因pageName='+pageName);
+            return;
+        }
+        if(!_this.language){
+            console.error('I18N解析失败，原因language='+_this.language);
+            return;
+        }
+        var key= '',
+        parseStr = '';
+        parseStr = str.replace(/{{i18n-(.+?)}}/g, function(t){
+            key = t.slice(7, t.length - 2);
+            try{
+                return _this.DATA[pageName][key][_this.language] || '';
+            }catch (e){
+                console.warn('language not found, key:'+ key +'----val'+ _this.language );
+                return '';
+            }
+        });
+        return parseStr;
+    }
+   
+    ,getText: function(pageName, key, v1, v2, v3){
+        var _this = this;
+        var intrusion = [];
+        if(arguments.length == 3 && typeof(arguments[2]) == 'object'){
+            intrusion = arguments[2];
+        }
+        else if(arguments.length > 2){
+            for(var i=2; i< arguments.length; i++){
+            intrusion.push(arguments[i]);
+            }
+        }
+        var _lg = '';
+        try{
+            _lg = _this.DATA[pageName][key][_this.language] || '';
+            if(!intrusion || intrusion.length == 0){
+                return _lg;
+            }
+            _lg = _lg.replace(/{d+}/g, function( word ){
+                return intrusion[word.match(/d+/)];
+            });
+            return _lg;
+        }catch (e){
+            console.warn('language not found, key:'+ key +'----val'+ _this.language );
+            return '';
+        }
+    }
+  };
+  
